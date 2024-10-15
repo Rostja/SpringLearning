36d112b376131f95b7031d5ceb3147ddd41cc316
Rostja <rasto.dorcak@gmail.com>
2024-10-15 23:03:29 +0200

    spring-boot-devtools changes settings >> compiler

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestConroller.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestConroller.java
@@ -11,4 +11,15 @@ public class FunRestConroller {
     public String sayHello(){
         return "Hello World";
     }
+
+    //expose a new endpoint for "workout"
+    @GetMapping("/workout")
+    public String getDailyWorkout(){
+        return "Run a hard 5k!";
+    }
+
+    @GetMapping("/fortune")
+    public String getDailyFortune(){
+        return "Today is your lucky day.";
+    }
 }
