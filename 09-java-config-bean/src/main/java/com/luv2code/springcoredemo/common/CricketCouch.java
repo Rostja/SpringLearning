35b507ca71de37740f961242746cf1721ca61863
Rostja <rasto.dorcak@gmail.com>
2024-10-19 21:32:00 +0200

    Setting of simple spring project

--- 09-java-config-bean/src/main/java/com/luv2code/springcoredemo/common/CricketCouch.java
+++ 09-java-config-bean/src/main/java/com/luv2code/springcoredemo/common/CricketCouch.java
@@ -14,14 +14,7 @@ public class CricketCouch implements Coach {
     }
 
     //define our init and destroy method
-    @PostConstruct
-    public void doMyStuff(){
-        System.out.println("In doMyStuff(): " + getClass().getSimpleName());
-    }
-    @PreDestroy
-    public void clearMyStuff() {
-        System.out.println("In clearMyStuff(): " + getClass().getSimpleName());
-    }
+
     @Override
     public String getDailyWorkout() {
         return "Practice fast bowling for 15 minutes :-)";
