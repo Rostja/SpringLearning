52bcc3bca256aacddcc400ac973b9a5366ac319f
Rostja <rasto.dorcak@gmail.com>
2024-10-31 19:15:46 +0100

    thymeleaf

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -3,13 +3,15 @@ package com.luv2code.springboot.thymeleafdemo.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
     // need a controller method to show initial HTML form
-    @RequestMapping("/showForm")
+    @GetMapping("/showForm")
     public String showForm(){
         return "helloworld-form";
     }
@@ -21,7 +23,7 @@ public class HelloWorldController {
     }
     //need a controller method to read form data and
     //add data to the model
-    @RequestMapping("/processFormVersionTwo")
+    @GetMapping("/processFormVersionTwo")
     public String letsShoutDude(HttpServletRequest request, Model theModel){
 
         //read the request parameter from the HTML form
