ffbb153d5bf5ea68e5c9333b2fef2182637dd2c2
Rostja <rasto.dorcak@gmail.com>
2024-11-05 20:10:50 +0100

    Delete data

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -46,4 +46,10 @@ public class EmployeeController {
 
         return "redirect:/employees/list";
     }
+    @GetMapping("/delete")
+    public String delete(@RequestParam("employeeId") int theId){
+       employeeService.deleteById(theId);
+
+       return "redirect:/employees/list";
+    }
 }
